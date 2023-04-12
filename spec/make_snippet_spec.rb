5641dd7c6647d109f1fec4323a676ad3f0059e2a
Sharmine <sharmine.ss@gmail.com>
2023-04-12 15:56:44 +0100

    renamed diary to make_snippet

--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -1,6 +1,6 @@
-require 'diary'
+require 'make_snippet'
 
-RSpec.describe "diary" do
+RSpec.describe "make_snippet" do
   it "returns the first five words of a long sentence" do
     result = make_snippet("one two three four five six")
     expect(result).to eq "one two three four five..."
